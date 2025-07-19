3b340466c2db3ff7c32cf31cf1db3b5cc98396e1
Xuejian Sundvall <jiansundvall@gmail.com>
2025-07-19 18:10:28 -0500

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
   import Container from "$lib/components/Container.svelte";
   import Content from "$lib/components/Content.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import { inject } from '@vercel/analytics';
+  inject();
 </script>
 
 <Container>
